8ffd3f99e144c885b9de285e94ae67931f12c4ff
caoyong <caoyong@jimi360.cn>
2017-11-07 16:24:06 +0800

    初始化项目

---
+++ 测试/测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试
+//
+//  Created by jimi2 on 2017/11/7.
+//  Copyright © 2017年 jimi2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试/测试/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  测试
+//
+//  Created by jimi2 on 2017/11/7.
+//  Copyright © 2017年 jimi2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
